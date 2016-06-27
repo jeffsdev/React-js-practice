9fa4bdc56f0cf1a58594e3b0d274b35b83e242ec
Jeff Seymour <seym1585@gmail.com>
2016-06-26 20:16:15 -0700

    add a bootstrap template to practice building a single page app

---
+++ src/js/pages/Layout.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link } from "react-router";
+
+export default class Layout extends React.Component {
+  navigate() {
+    this.props.history.pushState(null, "/");
+  }
+  render() {
+    return (
+      <div>
+        <h1>KillerNews.net</h1>
+        {this.props.children}
+        <Link to="archives">archives</Link>
+        <Link to="settings"><button class="btn btn-success">settings</button></Link>
+        <button onClick={this.navigate.bind(this)}>featured</button>
+      </div>
+    );
+  }
+}
